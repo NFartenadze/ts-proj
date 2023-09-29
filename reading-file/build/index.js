c22144f1499263d8e3332c67275dd09cfdbf4490
NFartenadze <nfartenadze@solvd.com>
2023-09-29 14:15:44 +0400

    added new project

---
+++ reading-file/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const MatchResults_1 = require("./MatchResults");
+const reader = new MatchReader_1.MatchReader("football.csv");
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResults_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResults_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
