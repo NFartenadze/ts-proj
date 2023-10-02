8ca6fbf3cc1e9de9044f2cd8f605184ccbdff2dd
NFartenadze <nfartenadze@solvd.com>
2023-10-02 11:53:45 +0400

    finished generics section

--- generics/generics.ts
+++ generics/generics.ts
@@ -1,7 +1,42 @@
-class ArrayOfNumbers {
-  constructor(public collection: number[]) {}
+class GenericArray<T> {
+  constructor(public collection: T[]) {}
 
-  get(index: number): number {
+  get(index: number): T {
     return this.collection[index];
+  }
+}
+
+const arr = new GenericArray(["a", "b", "c"]);
+
+//type inference with generics
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything([1, 2, 3]);
+
+//Generic constraints
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
   }
 }
